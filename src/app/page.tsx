f9c23dfa23dd078f949310642b0bf8d150a731bb
Facundo Pettersson <visualstudio.creator23@gmail.com>
2024-11-19 20:02:51 -0300

    feat: Create Footer and Header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
+import { Footer } from "@/components/Footer/Footer";
 import { Header } from "@/components/layout/Header";
 
 export default function Home() {
   return (
-    <main >
+    <>
       <Header />
-    </main>
+      <main className="h-screen">
+
+      </main>
+      <Footer />
+    </>
   );
 }
-
