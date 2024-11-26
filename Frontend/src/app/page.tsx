cfef138030d9f0b979954319fcc96660fe4083c0
Facundo Pettersson <visualstudio.creator23@gmail.com>
2024-11-26 17:43:57 -0300

    fix: Changes in the footer pages

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -1,5 +1,5 @@
 import { Footer } from "@/components/Footer/Footer";
-import { Header } from "@/components/layout/Header";
+import { Header } from "@/components/Layout/Header";
 
 export default function Home() {
   return (
