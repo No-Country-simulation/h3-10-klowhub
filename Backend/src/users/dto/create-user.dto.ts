974c05de4e7476dc35dbb5077c72ea17a8270c31
Hector Garcia <garcia27h@hotmail.com>
2024-11-27 15:13:25 -0300

    Feat: se agregó modulo de conexión a base de datos, y ruta de registro

--- Backend/src/users/dto/create-user.dto.ts
+++ Backend/src/users/dto/create-user.dto.ts
@@ -1,1 +1,25 @@
-export class CreateUserDto {}
+import { IsAlpha, IsEmail, IsNotEmpty, IsString, IsStrongPassword, MaxLength, MinLength } from 'class-validator'
+
+export class CreateUserDto {
+    @IsAlpha()
+    name: string
+
+    @IsAlpha()
+    lastname: string
+
+    @IsString()
+    birthday: string
+
+    @IsEmail()
+    email: string
+
+    @IsString()
+    profile_image: string
+
+    @MaxLength(8)
+    @MinLength(4)
+    @IsNotEmpty()
+    password: string
+
+    role_id: number // revisar si se va a manejar los roles dentro de la tabla user
+}
--- Backend/src/users/users.module.ts
+++ Backend/src/users/users.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { SharedModule } from 'src/shared-module/shared.module';
+import { PasswordService } from 'src/shared-module/password.service';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
+  imports : [PrismaModule, SharedModule]
 })
 export class UsersModule {}
