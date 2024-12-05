82ea677ee25369d722b4001ef4b61359de95d943
Facundo Pettersson <visualstudio.creator23@gmail.com>
2024-12-05 17:15:24 -0300

    fix: Deploy Vercel

--- Frontend/src/app/home/cursos/[course_name]/page.tsx
+++ Frontend/src/app/home/cursos/[course_name]/page.tsx
@@ -1,36 +1,28 @@
 'use client';
 
-import Page_Details_Course from '@/Pages/Detalles_Cursos/Page_Details_Course';
-import React, { useState, useEffect } from 'react';
+import  Page_Details_Course from '@/Pages/Detalles_Cursos/Page_Details_Course';
+import React from 'react';
 
 interface PageProps {
-  params: {
+  params: Promise<{
     course_name: string;
-  };
+  }>;
 }
 
 const CourseDetail = ({ params }: PageProps) => {
-  const [courseName, setCourseName] = useState<string | null>(null);
+  const { course_name } = React.use(params);
 
-  useEffect(() => {
-    const fetchParams = async () => {
-      const resolvedParams = await params; // Await if params is a Promise
-      setCourseName(resolvedParams.course_name);
-    };
+  console.log('Información del parámetro:', course_name);
 
-    fetchParams();
-  }, [params]);
-
-  if (!courseName) {
+  if (!course_name) {
     return <div>No se encontró el curso.</div>;
   }
 
-  console.log("Información del parámetro:", courseName);
-  console.log("Tipo de dato del course_name:", typeof courseName);
+  console.log("Tipo de dato del course_name: " + typeof course_name)
 
   return (
     <main className="flex justify-center">
-      <Page_Details_Course id={courseName} />
+      <Page_Details_Course id={course_name} />
     </main>
   );
 };
