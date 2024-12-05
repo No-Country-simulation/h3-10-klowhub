fb56d7f694a4c60bd39d4b64418a55e918e3b62b
nicolas ramirez <jnicoramirez@hotmail.com>
2024-12-05 00:31:28 -0300

    feat: :bug: Logout
    Logout Fixed

--- Frontend/src/components/Layout/Footer.tsx
+++ Frontend/src/components/Layout/Footer.tsx
@@ -3,22 +3,20 @@ import useAuth from "@/Hooks/useAuth";
 import { Icon_Facebook } from "../../../public/icons/Footer_Icon/Icon_Facebook";
 import { Icon_Linkendi } from "../../../public/icons/Footer_Icon/Icon_Linkendi";
 import { Icon_Twitter } from "../../../public/icons/Footer_Icon/Icon_Twitter";
+import { useContext, useEffect, useState } from "react";
 export function Footer() {
   const { logout, isLoggedIn } = useAuth();
-  const loggedIn = isLoggedIn;
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  useEffect(() => {
+    setLoggedIn(isLoggedIn); // Sincroniza el estado solo en el cliente
+  }, [isLoggedIn]);
+
   const handleLogout = () => {
     logout();
   };
   return (
     <>
-      {loggedIn && (
-        <button
-          onClick={handleLogout}
-          className="mt-4 px-4 py-2 bg-purple-700 text-white rounded hover:bg-purple-900 w-48"
-        >
-          Cerrar Sesión
-        </button>
-      )}
       <footer className="flex flex-col justify-center pt-10  h-auto bg-gradient-to-b from-[#201C2D] via-[#201D43] to-[#262136] mt-8">
         <section className="flex flex-col ml-6 gap-y-5 md:flex-row md:justify-evenly md:pt-2 lg:mb-32  ">
           <article className="flex flex-col gap-y-2 text-[14px] lg:text-[17px]">
@@ -50,6 +48,14 @@ export function Footer() {
             </div>
           </article>
         </section>
+        {loggedIn && (
+          <button
+            onClick={handleLogout}
+            className="mt-4 px-4 py-2 bg-purple-700 text-white rounded hover:bg-purple-900 w-48"
+          >
+            Cerrar Sesión
+          </button>
+        )}
         <section className="flex justify-center  pt-5 border-none  ">
           <h2 className="border-t-2 border-gray-500 w-[90%] md:w-full text-center pt-2 pb-2 text-[10px] md:text-[15px]">
             © Klowhub
